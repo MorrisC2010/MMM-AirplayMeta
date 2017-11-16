13f76a124bbed3f3bc5ffa374f389e740d4812d8
morrisc2010 <cmorris565@gmail.com>
2017-11-16 12:05:09 -0600

    updated main js file

--- MMM-AirplayMeta.js
+++ MMM-AirplayMeta.js
@@ -22,24 +22,37 @@ Module.register("MMM-AirplayMeta",{
 	},
 
 	textArray: function() {
-		var i = 0;
 		var textToShow;
 
-		if (i >= 3) {
+		textToShow = this.config.metaData.Song.slice(0);
+		return textToShow;
+	},
+
+	numberInit: function() {
+		var i = 0;
+		
+		if (i > 2) {
 			i=0;
 		} else {
 			i++;
 		}
-		textToShow = this.config.metaData.Song(i);
-		return textToShow;
+		return i;
+	},
+
+	textSwitcher: function() {
+		var song = this.textArray();
+		var index = this.numberInit();
+
+		return song[index];
 	},
 
 	// Override dom generator.
 	getDom: function() {
 
-		var metadataText = this.textArray();
+		var metadataText = this.textSwitcher();
 		var metadata =  document.createTextNode(metadataText); 
 		var wrapper = document.createElement("div");
+		wrapper.className = this.config.classes ? this.config.classes : "thin xlarge bright";
 		wrapper.appendChild(metadata);
 
 		return wrapper;
